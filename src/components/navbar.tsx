4102346e30378cbb593b841df1a70e5d636430f8
Keith <keithloh99@gmail.com>
2023-04-18 13:09:22 +0800

    Fix deployment bug

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return(
+    <nav className="  mx-auto rounded-lg border border-gray-200 bg-gray-50 py-3 px-5 shadow dark:border-gray-700 dark:bg-gray-800">
+      <div className=" mx-auto flex flex-wrap items-center  justify-between gap-x-5  md:flex-nowrap">
+          <div className="w-full flex-grow justify-between gap-x-2 md:flex">
+            
+          </div>
+          </div>
+    </nav>
+  );
+};
+
+export default Navbar;
