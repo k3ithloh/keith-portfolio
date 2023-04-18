e96e4d628ebf2825e48d7c9c7889a45a0a053709
Keith <keithloh99@gmail.com>
2023-04-18 18:34:49 +0800

    Fix a to Link nextjs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <nav className="  mx-auto rounded-lg border-b px-5 py-4 shadow dark:border-gray-800">
@@ -9,22 +11,22 @@ const Navbar = () => {
             </a>
           </div>
           <div className="flex flex-wrap items-center justify-between gap-x-2 md:flex-nowrap">
-            <a href="/" className="text-base font-medium">
+            <Link href="/" className="text-base font-medium">
               <span className="text-blue-600">01. </span>
               <span>About</span>
-            </a>
-            <a href="/about" className=" text-base font-medium">
+            </Link>
+            <Link href="/about" className=" text-base font-medium">
               <span className="text-blue-600">02. </span>
               <span>Experience</span>
-            </a>
-            <a href="/projects" className="text-base font-medium">
+            </Link>
+            <Link href="/projects" className="text-base font-medium">
             <span className="text-blue-600">03. </span>
             <span className="">Projects</span>
-            </a>
-            <a href="/projects" className="text-base font-medium">
+            </Link>
+            <Link href="/projects" className="text-base font-medium">
             <span className="text-blue-600">04. </span>
             <span className="">Contact</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
