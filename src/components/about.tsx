81483ea4b71946135dfc36c4d41b89b9f7be7e5d
Keith <keithloh99@gmail.com>
2023-05-01 16:52:41 +0800

    Update to Image tag from img for optimization

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,42 +1,204 @@
+import Image from "next/image";
 
 const About = () => {
   return (
     <div id="about" className="flex flex-col items-center">
-      <div className="mx-8 flex flex-wrap mt-28 w-full max-w-6xl">
-        <h1 className="mt-64 mb-4 font-bold text-2xl md:text-6xl text-white lg:text-6xl xl:text-7xl grow ml-8">About Me.</h1>
-        <div className="w-full grid grid-cols-2 -mt-20">
-          <div className="col-span-1 text-base md:text-lg text-ebony lg:text-xl xl:text-2xl content-center mt-28 ml-8"> I&apos;m a sophmore year Information Systems student at Singapore Management University, majoring in digital cloud solutions  and business analytics. A highly motivated individual with a keen eye for detail, who constantly strives to improve his skills.
-          <br></br><br></br>I&apos;m passionate about software development, web3, mechanical keyboards, photography and Formula 1.
-          <br></br> <br></br> Here are the technologies I&apos;ve been working with 
-          <br></br><br></br>
+      <div className="mx-8 mt-28 flex w-full max-w-6xl flex-wrap">
+        <h1 className="mb-4 ml-8 mt-64 grow text-2xl font-bold text-white md:text-6xl lg:text-6xl xl:text-7xl">
+          About Me.
+        </h1>
+        <div className="-mt-20 grid w-full grid-cols-2">
+          <div className="col-span-1 ml-8 mt-28 content-center text-base text-ebony md:text-lg lg:text-xl xl:text-2xl">
+            {" "}
+            I&apos;m a sophmore year Information Systems student at Singapore
+            Management University, majoring in digital cloud solutions and
+            business analytics. A highly motivated individual with a keen eye
+            for detail, who constantly strives to improve his skills.
+            <br></br>
+            <br></br>I&apos;m passionate about software development, web3,
+            mechanical keyboards, photography and Formula 1.
+            <br></br> <br></br> Here are the technologies I&apos;ve been working
+            with
+            <br></br>
+            <br></br>
             <div className="grid grid-cols-6 gap-6 ">
-              <img className="hover:animate-bounce h-12" src="/img/javascript.svg" alt="JS" />
-              <img className="hover:animate-bounce h-12" src="/img/typescript.svg" alt="TS" />
-              <img className="hover:animate-bounce h-12" src="/img/rust.svg" alt="Rust" />
-              <img className="hover:animate-bounce h-12" src="/img/react.svg" alt="React" />
-              <img className="hover:animate-bounce h-12" src="/img/vue.svg" alt="Vue" />
-              <img className="hover:animate-bounce h-12" src="/img/nextjs.svg" alt="NextJS" />
-              <img className="hover:animate-bounce h-12" src="/img/nodejs.svg" alt="NodeJS" />
-              <img className="hover:animate-bounce h-12" src="/img/express.svg" alt="Express" />
-              <img className="hover:animate-bounce h-12" src="/img/puppeteer.svg" alt="Puppeteer" />
-              <img className="hover:animate-bounce h-12" src="/img/python.svg" alt="Python" />
-              <img className="hover:animate-bounce h-12" src="/img/flask.svg" alt="Flask" />
-              <img className="hover:animate-bounce h-12" src="/img/dotnet.svg" alt="DotNet" />
-              <img className="hover:animate-bounce h-12" src="/img/tailwind.svg" alt="Tailwind CSS" />
-              <img className="hover:animate-bounce h-12" src="/img/bootstrap.svg" alt="Bootstrap" />
-              <img className="hover:animate-bounce h-12" src="/img/html-5.svg" alt="HTML-5" />
-              <img className="hover:animate-bounce h-12" src="/img/css-3.svg" alt="CSS-3" />
-              <img className="hover:animate-bounce h-12" src="/img/docker.svg" alt="Docker" />
-              <img className="hover:animate-bounce h-12" src="/img/rabbitmq.svg" alt="RabbitMq" />
-              <img className="hover:animate-bounce h-12" src="/img/postgresql.svg" alt="Postgresql" />
-              <img className="hover:animate-bounce h-12" src="/img/mysql.svg" alt="MySQL" />
-              <img className="hover:animate-bounce h-12" src="/img/mongo.svg" alt="Mongo" />
-              <img className="hover:animate-bounce h-12" src="/img/firebase.svg" alt="Firebase" />
-              <img className="hover:animate-bounce h-12" src="/img/figma.svg" alt="Figma" />
-              <img className="hover:animate-bounce h-12" src="/img/vercel.svg" alt="Vercel" />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/javascript.svg"
+                alt="JS"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/typescript.svg"
+                alt="TS"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/rust.svg"
+                alt="Rust"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/react.svg"
+                alt="React"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/vue.svg"
+                alt="Vue"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/nextjs.svg"
+                alt="NextJS"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/nodejs.svg"
+                alt="NodeJS"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/express.svg"
+                alt="Express"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/puppeteer.svg"
+                alt="Puppeteer"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/python.svg"
+                alt="Python"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/flask.svg"
+                alt="Flask"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/dotnet.svg"
+                alt="DotNet"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/tailwind.svg"
+                alt="Tailwind CSS"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/bootstrap.svg"
+                alt="Bootstrap"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/html-5.svg"
+                alt="HTML-5"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/css-3.svg"
+                alt="CSS-3"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/docker.svg"
+                alt="Docker"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/rabbitmq.svg"
+                alt="RabbitMq"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/postgresql.svg"
+                alt="Postgresql"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/mysql.svg"
+                alt="MySQL"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/mongo.svg"
+                alt="Mongo"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/firebase.svg"
+                alt="Firebase"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/figma.svg"
+                alt="Figma"
+              />
+              <Image
+                className="h-12 hover:animate-bounce"
+                width={200}
+                height={200}
+                src="/img/vercel.svg"
+                alt="Vercel"
+              />
             </div>
           </div>
-            <img className="ml-12 scale-75 hover:scale-90 ease-in-out duration-300" src="/img/keith.JPG" alt="keith" />
+          <Image
+            className="ml-12 scale-75 duration-300 ease-in-out hover:scale-90"
+            width={800}
+            height={800}
+            src="/img/keith.JPG"
+            alt="keith"
+          />
         </div>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-// import { Nunito } from 'next/font/google';
 import localFont from "next/font/local";
 import Navbar from "~/components/navbar";
 import Footer from "~/components/footer";
@@ -12,8 +11,6 @@ import About from "~/components/about";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
-// add Nunito font
-// const nunito = Nunito({ subsets: ['latin'] });
 
 // add CustomFont
 const askpekta = localFont({
