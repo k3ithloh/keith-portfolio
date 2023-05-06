1590ad40c55fa2b8cc6ff05bf28f51bd4d01f1db
Keith <keithloh99@gmail.com>
2023-05-06 23:24:03 +0800

    Add social media icons to footer on sm screen size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
-
+import config, { SocialMedia } from '~/config';
+import Icon from './icons/icons';
 
 const Footer = () => {
   return (
-    
-    <footer className="mx-auto flex rounded-lg px-12 py-6">      
-      <div className=" mx-auto flex flex-wrap items-center  justify-between gap-x-5  md:flex-nowrap">
+    <footer className="mx-auto flex flex-col rounded-lg px-12 py-6">  
+      <div className="visible md:invisible flex flex-wrap items-center justify-center gap-x-5">
+          {config.socialMedia &&
+          config.socialMedia.map(({ url, name }: SocialMedia, i) => (
+            <li className="list-none mb-3" key={i}>
+              <Link href={url} aria-label={name} target="_blank" rel="noopener noreferrer">
+                <Icon name={name} />
+              </Link>
+            </li>
+          ))}
+      </div>    
+      <div className="mx-auto flex flex-wrap items-center justify-center gap-x-5  md:flex-nowrap">
         <Link
           href="/https://github.com/k3ithloh/keith-portfolio"
           className="text-sm text-ebony hover:text-white"
