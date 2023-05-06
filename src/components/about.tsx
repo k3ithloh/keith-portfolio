0eac3c557eeab7087d9e5623516c9d9b243f11e3
Keith <keithloh99@gmail.com>
2023-05-06 22:17:11 +0800

    Fixed conflict

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div id="about" className="flex flex-col justify-center items-center px-28 h-[95vh]">
-      <div className="flex max-w-6xl flex-wrap">
-        <h1 className="mb-2 lg:mb-0 grow text-2xl font-bold text-white md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+    <div id="about" className="flex flex-col items-center px-32">
+      <div className="mx-8 mt-20 flex w-full max-w-6xl flex-wrap">
+        <h1 className="mb-4 ml-8 mt-64 grow text-5xl font-bold text-white md:text-6xl lg:text-6xl xl:text-7xl">
           About Me.
         </h1>
-        <div className=" grid w-full grid-cols-1 lg:grid-cols-2 place-items-center ">
-          <div className="col-span-1 mt-0 md:mt-12 content-center text-sm text-ebony md:text-base lg:text-lg xl:text-xl 2xl:text-2xl pb-12 md:pb-20">
+        <div className=" grid w-full grid-cols-1 place-items-center lg:grid-cols-2 ">
+          <div className="col-span-1 mt-0 content-center pb-12 text-sm text-ebony md:mt-12 md:pb-20 md:text-base lg:text-lg xl:text-xl">
             {" "}
             I&apos;m a sophmore year Information Systems student at Singapore
             Management University, majoring in digital cloud solutions and
@@ -18,17 +18,15 @@ const About = () => {
             <br></br>I&apos;m passionate about software development, web3,
             mechanical keyboards, photography and Formula 1.
           </div>
-          <div className="place-self-center relative w-full h-auto sm:w-2/4 md:w-2/4 lg:w-1/2 xl:w-3/5 lg:duration-300 ease-in-out lg:hover:scale-125">
-          <Image
-            className=""
-            // fill={true}
-              width="800"
-              height="800"
-            src="/img/keith.JPG"
-            alt="keith"
-          />
+          <div className="place-self-center">
+            <Image
+              className=" h-64 w-auto ease-in-out md:h-72 lg:h-96 lg:duration-300 lg:hover:scale-125"
+              width={800}
+              height={800}
+              src="/img/keith.JPG"
+              alt="keith"
+            />
           </div>
-          
         </div>
       </div>
     </div>
